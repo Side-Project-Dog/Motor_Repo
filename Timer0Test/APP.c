2777abf6bcdbff96bd654af461c63299a25a9372
Mohammmedibra96 <mohammedibra96@gmail.com>
2020-02-19 17:15:34 +0200

    commit the application
    Signed-off-by: Mohammmedibra96 <mohammedibra96@gmail.com>

--- Timer0Test/APP.c
+++ Timer0Test/APP.c
@@ -13,76 +13,60 @@
 #include<avr/delay.h>
 #include"Timer0.h"
 #include"CLCD_interfac.h"
+#include"DIO_register.h"
+#include"motor.h"
 
 
+/*
+ * ISR function of the timer
+ * tick time=1msec
+ * 1 sec high
+ * 1 sec low
+ * duty cycle 50%
+ *
+ *
+ *if you need to add another pin to out PWM you should write more lines in the below function only
+ *
+ *
+ * */
 
-volatile u32 counter=0,c1,c2,c3,flag=0;
 
-void __vector_6 (void)  __attribute__((signal,__INTR_ATTRS));
-void __vector_6 (void)
+static u32 Timer0_OV_Counter=0;
+static u32 Counter=0;
+u32 Duty1=1000;
+void LEDON(void)
 {
-	if (flag==0)
+	Timer0_OV_Counter++;
+	if(22==Timer0_OV_Counter)   //tick time 1 msec
 	{
-		c1=ICR1;
-		counter=0;
-		CLR_BIT(TCCR1B,6);
-	}
-	else if(flag==1)
-	{
-		c2=ICR1+(counter*65536);
-		SET_BIT(TCCR1B,6);
+		Timer0_OV_Counter=0;
+		Counter++;
 	}
 
-	else if(flag==2)
+//da example lw 3aiza tzwdo pins zwdin zi el condition eli ta7t
+//////////////////////////////////////////////////////////
+	if(Counter<=Duty1)   //(Duty 1 sec)   (1000 counts *1msec(tick time) =1sec)
 	{
-		c3=ICR1+(counter*65536);
-		CLR_BIT(TIMSK,5);//=0b11011111;// close the ICE interrupt
-
+		SetPinValue('A',0,1);
 	}
-	flag++;
+	else if(Counter==2000) //period 2 sec
+	{
+		Counter=0;
+	}
+	else  ///1 sec low
+		SetPinValue('A',0,0);
+///////////////////////////////////////////////
+
+
 }
-
-
-
-
-void __vector_9 (void)  __attribute__((signal,__INTR_ATTRS));
-void __vector_9 (void)
-{
-	counter++;
-}
-
 
 int main (void)
 {
 	PortInitialization();
-//	CLCD_voidInitalization();
-//	CLCD_voidSetCursor(0,0);
-//	CLCD_voidWriteString("Duty: ");
-	Timer0_voidInit();
-	timer1_pwm_init();
-
+	MOTOR_Init(LEDON);
 	while(1)
 	{
-//		if(flag==3)
-//		{
-//			u32 duty=((c2-c1)*100/(c3-c1));
-//			u32 freq=(8000000/(c3-c1));
-//
-//			//LCD_writecommand(1);
-//			CLCD_voidSetCursor(0,0);
-//			CLCD_voidWriteString("Duty: ");
-//			//LCD_setposition(0,5);
-//			LCDWriteInt(duty);
-//
-//			CLCD_voidSetCursor(1,0);
-//			CLCD_voidWriteString("Freq: ");
-//			//LCD_setposition(1,5);
-//			LCDWriteInt(freq);
-//
-//			flag=0;
-//			SET_BIT(TIFR,5); // to clear the Input Capture Flag "bit 5"
-//			SET_BIT(TIMSK,5);// to re-enable the Input Capture Interrupt "bit 5"
-//		}
+
 	}
 	return -1;
 }
